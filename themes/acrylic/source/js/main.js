f0ab857e8595ffaa2c8632ee90c60890ddefce0d
ROYIANS <1294686101@qq.com>
2023-04-06 08:48:02 +0800

    async: 同步稳定版

--- themes/acrylic/source/js/main.js
+++ themes/acrylic/source/js/main.js
@@ -159,8 +159,12 @@ const percent = () => {
 
 class toc {
     static init() {
+        const tocContainer = document.getElementById('card-toc')
+        if (!tocContainer || !tocContainer.querySelector('.toc a')) {
+            tocContainer.style.display = 'none'
+            return
+        }
         const el = document.querySelectorAll('.toc a')
-        if (!el) return
         el.forEach((e) => {
             e.addEventListener('click', (event) => {
                 event.preventDefault()
@@ -390,14 +394,20 @@ class tabs {
     }
 }
 
+const setTheme = () => {
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+};
+
 window.refreshFn = () => {
     scrollFn()
     sidebarFn()
     setTimeState()
     chageTimeFormate()
+    setTheme()
     acrylic.addRuntime()
     GLOBALCONFIG.lazyload.enable && acrylic.lazyloadImg()
-    GLOBALCONFIG.lightbox && acrylic.lightbox('#article-container img, #bber .bber-content-img img')
+    GLOBALCONFIG.lightbox && acrylic.lightbox('#article-container img, #bber .bber-content-img img, #album_detail album-content-img img')
     GLOBALCONFIG.randomlinks && randomLinksList()
     PAGECONFIG.toc && toc.init()
     if (PAGECONFIG.is_post) {
@@ -410,6 +420,9 @@ window.refreshFn = () => {
         acrylic.initbbtalk()
     }
     if (PAGECONFIG.is_page && PAGECONFIG.page === 'says') acrylic.reflashEssayWaterFall()
+    if (PAGECONFIG.is_page) {
+        if (document.getElementById('album_detail')) acrylic.reflashEssayWaterFall()
+    }
     GLOBALCONFIG.covercolor && coverColor()
 }
 
