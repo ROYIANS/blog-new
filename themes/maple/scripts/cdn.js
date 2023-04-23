5de0bccdf96c9b31c089c47d07aff0184963d1af
ROYIANS <1294686101@qq.com>
2023-04-23 18:33:24 +0800

    feat: 修改

--- themes/maple/scripts/cdn.js
+++ themes/maple/scripts/cdn.js
@@ -24,6 +24,11 @@ hexo.extend.filter.register('before_generate', () => {
       file: 'js/utils.js',
       version
     },
+    translate: {
+      name: 'hexo-theme-dreamland-book',
+      file: 'js/tw_cn.js',
+      version
+    },
     heti_custom_css: {
       name: 'hexo-theme-dreamland-book',
       file: 'css/heti/heti.css',
